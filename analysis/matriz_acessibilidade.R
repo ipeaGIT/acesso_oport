1833eab4a3799b068c4aefe49e4805ac0f38c1e1
Kauê Braga <kauebraga90@gmail.com>
2019-03-22 14:23:42 -0300

    changes..

--- analysis/matriz_acessibilidade.R
+++ analysis/matriz_acessibilidade.R
@@ -3,82 +3,166 @@ library(tidyr)
 library(opentripplanner)
 library(sf)
 library(purrr)
-source("R/sfc_as_cols.R")
+library(httr)
+library(data.table)
+library(furrr)
+library(parallel)
 
 
-# ABRIR ARQUIVOS ----------------------------------------------------------
-
-for_hex <- st_read("data/hex_municipio/fortaleza/hex_fortaleza.shp", crs = 4326) %>%
-  mutate(id = 1:n()) %>%
-  filter(id %in% c(1:200)) %>%
-  select(id)
-
-for_hex_centroids <- for_hex %>%
-  st_centroid() %>%
-  sfc_as_cols()
-
-# dados de escolas
+# FUNCAO PARA CONSTRUIR GRAPH (SO RODAR UMA VEZ!) -------------------------
 
 
-# LIGAR O OTP -------------------------------------------------------------
 
-otp_setup(otp = "otp/programs/otp.jar", dir = "otp", router = "fortaleza")
-otp_for <- otp_connect(router = "fortaleza")
+construir_graph <- function(cidade) {
+  
+  # Os arquivos de gtfs e .obj devem estar na pasta "cidade"
+  
+  otp_build_graph(otp = "../otp/programs/otp.jar", dir = "../otp", router = cidade) 
+  
+}
 
-
-# GERAR TABELA DE MATRIZ --------------------------------------------------
-
-for_od <- for_hex_centroids %>%
-  expand(id, id) %>%
-  left_join(for_hex_centroids) %>%
-  left_join(for_hex_centroids, by = c("id1" = "id"), suffix = c(".origem", ".destino")) %>%
-  rename(origem = id, destino = id1)
+# construir_graph("fortaleza")
 
 
-# GERAR LISTA COM COORDENADAS ---------------------------------------------
+
+# FUNCAO PARA LIGAR SERVIDOR DO OTP DA CIDADE -----------------------------
 
 
-fromPlace <- map2(for_od$lat.origem, for_od$lon.origem, c)
-toPlace <- map2(for_od$lat.destino, for_od$lon.destino, c)
 
-names(fromPlace) <- 1:length(fromPlace)
-names(toPlace) <- 1:length(toPlace)
+ligar_servidor <- function(cidade) {
+  
+  otp_setup(otp = "../otp/programs/otp.jar", dir = "../otp", router = cidade)
+  otp_for <- otp_connect(router = cidade)
+  
+}
 
-
-# PASSAR PARA O OTP -------------------------------------------------------
-
-a <- Sys.time()
-for_matriz_access <- map2(fromPlace[1:714], toPlace[1:714], .f = otp_plan, otpcon = otp_for,
-                          date_time = as.POSIXct("2018-11-05"), mode = "TRANSIT")
-b <- Sys.time()
-
-# Identificar 
-names(for_matriz_access) <- paste(rep(1:2, each = 357), rep(1:357, 2), sep = "_")
-
-sao_df <- map_chr(for_matriz_access, is.data.frame)
-
-sao_df_num <- which(sao_df == "TRUE")
-
-quequeu <- for_matriz_access[sao_df_num]
-
-opaaaa <- map(quequeu, st_set_geometry, NULL)
-
-vaaai <- bind_rows(opaaaa, .id = "origem_destino") %>%
-  group_by(origem_destino) %>%
-  filter(route_option == 1) %>%
-  mutate(etapa = 1:n()) %>%
-  select(origem_destino, etapa, modo = mode, duration, walkTime, transitTime, waitingTime)
+ligar_servidor("fortaleza")
 
 
-# OTP BATCH ---------------------------------------------------------------
 
-origens_matrix <- as.matrix(for_hex_centroids)
+# 
+# data <- "data/hex_municipio/fortaleza/hex_fortaleza.shp"
+# cidade <- "fortaleza"
+# 
+# data <- "data/hex_municipio/rio_de_janeiro/hex_rio_de_janeiro.shp"
+# cidade <- "rio"
 
-origens_matrix <- origens_matrix[,c(1,3,2)]
-head(origens_matrix)
-colnames(origens_matrix) <- c("id_hex", "latitude", "longitude" )
 
-system.time(
-otp_plan_batch_teste <- otp_plan_batch(otpcon = otp_for, 
-                                       fromPlace = origens_matrix[1:2,], toPlace = origens_matrix[],
-                                       date_time = as.POSIXct("2018-11-05"), mode = "TRANSIT"))
+
+# FUNCAO PARA PRODUZIR MATRIZ DE IMPEDANCIA -------------------------------
+
+
+otp_vai <- function(data, cidade) {
+  
+  source("R/sfc_as_cols.R")
+  
+  # ABRIR ARQUIVOS ----------------------------------------------------------
+  
+  for_hex <- st_read(data, crs = 4326) %>%
+    mutate(id = 1:n()) %>%
+    select(id) %>%
+    # filter(id %in% c(1:10)) %>%
+    identity()
+  
+  for_hex_centroids <- for_hex %>%
+    st_centroid() %>%
+    sfc_as_cols()
+  
+  
+  # GERAR TABELA DE MATRIZ --------------------------------------------------
+  
+  for_od <- for_hex_centroids %>%
+    expand(id, id) %>%
+    left_join(for_hex_centroids) %>%
+    left_join(for_hex_centroids, by = c("id1" = "id"), suffix = c(".origem", ".destino")) %>%
+    rename(origem = id, destino = id1)
+  
+  
+  # GERAR LISTA COM COORDENADAS ---------------------------------------------
+  
+  
+  origem <- map2(for_od$lat.origem, for_od$lon.origem, c)
+  destino <- map2(for_od$lat.destino, for_od$lon.destino, c)
+  
+  names(origem) <- 1:length(origem)
+  names(destino) <- 1:length(destino)
+  
+  
+  url <- paste0("http://localhost:8080/otp/routers/", cidade, "/plan")
+  
+  request_url <- function(origem, destino, vai) {
+    
+    # TRATAR AS COORDENADAS ---------------------------------------------------
+  
+    fromPlace <- paste0(origem, collapse = ",")
+    toPlace <- paste0(destino, collapse = ",")
+    
+
+    # MAKE REQUEST ------------------------------------------------------------
+
+    req <- httr::GET(
+      vai,
+      query = list(
+        fromPlace = fromPlace,
+        toPlace = toPlace,
+        mode = "TRANSIT,WALK",
+        date = "11-05-2018",
+        time = "11:00am",
+        maxWalkDistance = "1000",
+        walkReluctance = "2",
+        arriveBy = "FALSE",
+        transferPenalty = "0",
+        minTransferTime = "0"
+      )
+    )
+    
+    text <- httr::content(req, as = "text", encoding = "UTF-8")
+    
+    x <- jsonlite::fromJSON(text)
+    
+  }
+  
+  plan(multiprocess)
+  finni <- future_map2(origem, destino, request_url, vai = url, .progress = TRUE)
+
+  names(finni) <- paste(rep(1:length(for_hex$id), each = length(for_hex$id)),
+                        rep(1:length(for_hex$id), length(for_hex$id)), sep = "_")
+  
+
+  # FUNCAO PARA ACESSAR CONTEUDO DA CONSULTA --------------------------------
+
+  acessar_consulta <- function(list.consulta) {
+
+    if (is.data.frame(list.consulta[["plan"]][["itineraries"]])) {
+
+      df <- list.consulta[["plan"]][["itineraries"]] %>%
+        as.data.frame() %>%
+        select(duration, walkTime, transitTime, waitingTime, transfers) %>%
+        mutate(option = 1:n())
+
+    } else {
+
+      df <- data.frame(duration = 0, walkTime = 0, transitTime = 0, waitingTime = 0, transfers = 0,
+                       option = 0)
+
+    }
+
+
+  }
+
+  fin_v1 <- future_map(finni, acessar_consulta) %>%
+    rbindlist(idcol="origem_destino") %>%
+    mutate_at(c("duration", "walkTime", "transitTime", "waitingTime"), ~ round(./60, digits = 1)) %>%
+    separate(origem_destino, c("id_origem", "id_destino"), sep = "_")
+
+
+}
+
+
+
+# TESTE !!!!!!!!!1 --------------------------------------------------------
+
+tictoc::tic()
+orra <- otp_vai("../data/hex_municipio/fortaleza/hex_fortaleza.shp", "fortaleza")
+tictoc::toc()
+
