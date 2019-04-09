f8027b9577cbb318191f1478e079f02348ff3770
Kauê Braga <kauebraga90@gmail.com>
2019-04-09 18:04:32 -0300

    add as demais resolucoes

--- R/agrupar_variaveis.R
+++ R/agrupar_variaveis.R
@@ -39,51 +39,63 @@
 
 
 
-agrupar_variaveis <- function(municipio, salvar_no_disco = TRUE) {
+agrupar_variaveis <- function(muni_shortname) {
   
-  muni_shortname <- substring(muni, 1, 3)
   
-  dir_muni <- paste0("../data/hex_municipio/hex_", muni_shortname, ".rds")
+  dir <- dir("../data/hex_municipio/", pattern = muni_shortname)
+  
+  res <- str_extract(dir, "\\d+")
+  
+  dir_muni <- paste0("../data/hex_municipio/hex_", muni_shortname, "_", res, ".rds")
+  
+  seila <- function(muni_res) {
     
-  hex_muni <- readRDS(dir_muni)
-  
-  cnes <- read_csv("../data-raw/hospitais/cnesnone_2018.csv") %>%
-    st_as_sf(coords = c("long", "lat"), crs = 4326)
-  
-  escolas <- read_csv("../data/censo_escolar/censo_escolar_2015.csv") %>%
-    filter(!is.na(lon)) %>%
-    # mutate(municipio == tolower(municipio)) %>%
-    # filter(municipio == muni) %>%
-    st_as_sf(coords = c("lon", "lat"), crs = 4326)
-  
-  dir_grade <- paste0("../data/grade_municipio/grade_", muni_shortname, ".rds")
-  
-  pop <- read_rds(dir_grade) %>%
-    select(id_grade, POP) %>%
-    st_centroid()
-  
-  
-  hex_muni_fim <- hex_muni %>%
-    st_join(pop) %>%
-    group_by(id_hex) %>%
-    summarise(pop_total = sum(POP)) %>%
-    ungroup() %>%
-    st_join(cnes) %>%
-    group_by(id_hex, pop_total) %>%
-    summarise(saude_total = n()) %>%
-    ungroup() %>%
-    st_join(escolas) %>%
-    group_by(id_hex, pop_total, saude_total) %>%
-    summarise(escolas_total = n())
-  
-  if (salvar_no_disco == TRUE) {
+    dir_muni <- muni_res
     
-    dir_output <- sprintf("../data/hex_agregados/hex_agregado_%s.rds", muni_shortname)
+    res <- str_extract(dir_muni, "\\d+")
     
-    write_rds(hex_muni_fim, dir_output)
+    hex_muni <- readRDS(dir_muni)
+    
+    cnes <- read_csv("../data-raw/hospitais/cnesnone_2018.csv") %>%
+      st_as_sf(coords = c("long", "lat"), crs = 4326)
+    
+    escolas <- read_csv("../data/censo_escolar/censo_escolar_2015.csv") %>%
+      filter(!is.na(lon)) %>%
+      # mutate(municipio == tolower(municipio)) %>%
+      # filter(municipio == muni) %>%
+      st_as_sf(coords = c("lon", "lat"), crs = 4326)
+    
+    dir_grade <- paste0("../data/grade_municipio/grade_", muni_shortname, ".rds")
+    
+    pop <- read_rds(dir_grade) %>%
+      select(id_grade, POP) %>%
+      st_centroid()
     
     
+    hex_muni_fim <- hex_muni %>%
+      st_join(pop) %>%
+      group_by(id_hex) %>%
+      summarise(pop_total = sum(POP)) %>%
+      ungroup() %>%
+      st_join(cnes) %>%
+      group_by(id_hex, pop_total) %>%
+      summarise(saude_total = n()) %>%
+      ungroup() %>%
+      st_join(escolas) %>%
+      group_by(id_hex, pop_total, saude_total) %>%
+      summarise(escolas_total = n())
+    
+      
+      dir_output <- sprintf("../data/hex_agregados/hex_agregado_%s_%s.rds", muni_shortname, res)
+      
+      write_rds(hex_muni_fim, dir_output)
+      
   }
   
+    walk(dir_muni, seila)
   
 }
+
+
+# agrupar_variaveis("for")
+
