fde2d18e9076597c0d0c153fe089ad92a5f3ea24
Kauê Braga <kauebraga90@gmail.com>
2019-04-11 18:26:26 -0300

    inicial

---
+++ R/extrair_linhas_altacapacidade.R
@@ -0,0 +1,135 @@
+
+# CORREDORES DE ALTA E MÉDIA CAPACIDADE DOS ARQUIVOS DE GT --------
+
+# comecar por bel
+
+library(tidytransit)
+library(dplyr)
+library(sf)
+library(mapview)
+library(sp)
+library(maptools)
+
+gtfs.bel <- read_gtfs("../otp/graphs/bel/gtfs_bhtransit_20181115.zip", local = TRUE)
+# gtfs.for <- read_gtfs("../otp/graphs/for/gtfs.zip", local = TRUE)
+
+
+gtfs.bel[["routes"]] %>%
+  filter(grepl("move", route_long_name, ignore.case = T)) %>%
+  View()
+
+routes_high <- gtfs.bel[["routes"]] %>%
+  filter(route_type %in% c(0, 1, 2))
+
+# arquivo de BEL nao tem shapes!!!!!!!!!!!!!!
+# shape_high <- gtfs.bel[["shapes"]]
+
+# entao checar nos trips..
+trips_high <- gtfs.bel[["trips"]] %>%
+  filter(route_id %in% routes_high$route_id)
+
+# entao checar no stop_times
+stop_times_high <- gtfs.bel[["stop_times"]] %>%
+  filter(trip_id %in% trips_high$trip_id) %>%
+  # pegar uma trip que seja em hora pico (consultar a tabela)
+  filter(trip_id == "METRO 011010020700") %>%
+  select(trip_id, stop_id, stop_sequence)
+
+# agora, pegar as paradas referentes a trip das linhas de alta capacidade
+stops_high <- gtfs.bel[["stops"]] %>%
+  select(stop_id, stop_lon, stop_lat) %>%
+  # filter(stop_id %in% stop_times_high$stop_id) %>%
+  right_join(stop_times_high)
+
+
+
+shapes_high <- stops_high %>%
+  points_to_line(long = "stop_lon", lat = "stop_lat", sort_field = "stop_sequence") %>%
+  mapview()
+
+
+
+
+# PARA O RIO DE JANEIRO ---------------------------------------------------
+
+
+
+gtfs.rio <- read_gtfs("../otp/graphs/rio/GTFS Rio feed_20190219.zip", local = TRUE)
+
+
+# gtfs.bel[["routes"]] %>% View()
+
+routes_high <- gtfs.rio[["routes"]] %>%
+  filter(route_type %in% c(0, 1, 2))
+
+# tem BRT?
+gtfs.rio[["routes"]] %>%
+  filter(grepl("BRT", route_long_name))
+
+# ir atras no trips
+trips_high <- gtfs.rio[["trips"]] %>%
+  filter(route_id %in% routes_high$route_id) %>%
+  distinct(shape_id)
+
+
+# ir atras no shapes
+shapes_high <- gtfs.rio[["shapes"]] %>%
+  filter(shape_id %in% trips_high$shape_id)
+
+# aplicar funcao
+
+shapes_high %>%
+  points_to_line_sf(long = "shape_pt_lon", lat = "shape_pt_lat", 
+                 id_field = "shape_id", sort_field = "shape_pt_sequence") %>%
+  st_as_sf(crs = 4326) %>%
+  st_set_crs(4326) %>%
+  mutate(shape_id = routes_high$shape_id) %>%
+  mapview()
+
+
+
+
+# FUNCAO ------------------------------------------------------------------
+
+extrair_HMcorredores <- function(gtfs_file) {
+  
+  gtfs.rio <- read_gtfs(gtfs_file, local = TRUE)
+  
+  
+  # gtfs.bel[["routes"]] %>% View()
+  
+  routes_high <- gtfs.rio[["routes"]] %>%
+    filter(route_type %in% c(0, 1, 2))
+  
+  # tem BRT?
+  gtfs.rio[["routes"]] %>%
+    filter(grepl("BRT", route_long_name))
+  
+  # ir atras no trips
+  trips_high <- gtfs.rio[["trips"]] %>%
+    filter(route_id %in% routes_high$route_id) %>%
+    distinct(shape_id)
+  
+  
+  # ir atras no shapes
+  shapes_high <- gtfs.rio[["shapes"]] %>%
+    filter(shape_id %in% trips_high$shape_id)
+  
+  # aplicar funcao
+  
+  source("R/points_to_line.R")
+  corredores <- shapes_high %>%
+    points_to_line_sf(long = "shape_pt_lon", lat = "shape_pt_lat", 
+                   id_field = "shape_id", sort_field = "shape_pt_sequence") %>%
+    mutate(shape_id = routes_high$shape_id)
+
+} 
+
+
+
+vai_for <- extrair_HMcorredores("../otp/graphs/rio/GTFS Rio feed_20190219.zip")
+
+
+  
+  
+  
---
+++ R/points_to_line.R
@@ -0,0 +1,47 @@
+
+# adaptado de https://rpubs.com/walkerke/points_to_line
+
+points_to_line_sf <- function(data, long, lat, id_field = NULL, sort_field = NULL) {
+  
+  # Convert to SpatialPointsDataFrame
+  coordinates(data) <- c(long, lat)
+  
+  # If there is a sort field...
+  if (!is.null(sort_field)) {
+    if (!is.null(id_field)) {
+      data <- data[order(data[[id_field]], data[[sort_field]]), ]
+    } else {
+      data <- data[order(data[[sort_field]]), ]
+    }
+  }
+  
+  # If there is only one path...
+  if (is.null(id_field)) {
+    
+    lines <- SpatialLines(list(Lines(list(Line(data)), "id")))
+    
+    return(lines)
+    
+    # Now, if we have multiple lines...
+  } else if (!is.null(id_field)) {  
+    
+    # Split into a list by ID field
+    paths <- sp::split(data, data[[id_field]])
+    
+    sp_lines <- SpatialLines(list(Lines(list(Line(paths[[1]])), "line1")))
+    
+    # I like for loops, what can I say...
+    for (p in 2:length(paths)) {
+      id <- paste0("line", as.character(p))
+      l <- SpatialLines(list(Lines(list(Line(paths[[p]])), id)))
+      sp_lines <- spRbind(sp_lines, l)
+    }
+    
+    
+    sp_lines <- sp_lines %>%
+      st_as_sf(crs = 4326) %>%
+      st_set_crs(4326)
+    
+    return(sp_lines)
+  }
+}
