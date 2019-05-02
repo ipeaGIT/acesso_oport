3fed43dfc6fe1c302af7886d3675f62970f7a212
Kauê Braga <kauebraga90@gmail.com>
2019-05-02 16:10:01 -0300

    add testes

--- analysis/teste_setores_para_hex.R
+++ analysis/teste_setores_para_hex.R
@@ -55,12 +55,14 @@ setor_para_hex <- function(setor_path, hex_path) {
   setor <- read_rds(setor_path) %>%
     filter(muni == "FORTALEZA") %>%
     mutate(id_setor = 1:n()) %>%
-    mutate(area_total = st_area(.)) %>%
-    dplyr::select(id_setor, renda_total, moradores_total, area_total)
+    mutate(area_setor = st_area(.)) %>%
+    dplyr::select(id_setor, renda_total, area_setor)
   
   # abrir grade
   grade <- read_rds(grade_path) %>%
-    dplyr::select(id_grade, pop_total = POP)
+    mutate(area_grade = st_area(.)) %>%
+    mutate(id_grade = 1:n()) %>%
+    dplyr::select(id_grade, pop_total = POP, area_grade)
   # mutate(pop_total = ifelse(is.na(pop_total), 0, pop_total))
   
   
@@ -69,43 +71,143 @@ setor_para_hex <- function(setor_path, hex_path) {
   #   dplyr::select(id_grade, pop_total = POP)
   # # mutate(pop_total = ifelse(is.na(pop_total), 0, pop_total))
   # 
-  # # quais sao os setores que incorporam completamente mais de uma grade?
-  # aaah <- st_contains_properly(setor, grade) %>% as.list()
-  # names(aaah) <- 1:length(aaah) 
-  # 
-  # que <- data.frame(id_setor = rep(names(aaah), map_dbl(aaah, length)),
-  #                   id_grade = unlist(aaah[c(which(lengths(aaah) > 0))]))
+  # quais sao os setores que incorporam completamente mais de uma grade?
+  aaah <- st_contains_properly(setor, grade) %>% as.list()
+  names(aaah) <- 1:length(aaah)
+
+  que <- data.frame(id_setor = rep(names(aaah), map_dbl(aaah, length)),
+                    id_grade = unlist(aaah[c(which(lengths(aaah) > 0))]))
   # 
   # 
   # setor_novo <- setor %>%
   #   filter(id_setor %in% que$id_setor)
   
-  # agora comeca tudo
+  # Para as quadriculas que estao completamente inseridas em um setor (tipo 1):
   
-  ui <- st_intersection(grade, setor) %>%
+  ui_tipo1 <- grade %>%
+    filter(id_grade %in% que$id_grade)
+  
+  # viz
+  mapview(ui_tipo1) + mapview(setor %>% filter(id_setor %in% que$id_setor))
+  
+  ui_tipo1_v2 <- ui_tipo1 %>%
+    st_intersection(setor) %>%
     # tip from https://rpubs.com/rural_gis/255550
-    mutate(area = st_area(.)) %>%
-    mutate(prop_area = area/area_total) %>%
-    mutate(pop_prop_grade = prop_area * pop_total) %>%
-    # mutate(prop_pop_setor = prop_area * moradores_total)
+    # Calcular a area de cada pedaco
+    mutate(area_pedaco = st_area(.)) %>%
+    # Calcular a proporcao de cada setor que esta naquele pedaco (essa sera a area a ponderar pela renda)
+    mutate(area_prop_setor = area_pedaco/area_setor) %>%
+    # # Calcular a proporcao de cada grade que esta naquele pedacao
+    # mutate(area_prop_grade =  area_pedaco/area_grade) %>%
+    # # Calcular a quantidade de populacao em cada pedaco (baseado na grade)
+    # mutate(pop_prop_grade = pop_total * area_prop_grade) %>%
+    # Calcular a proporcao de populacao de cada grade que esta dentro do setor
     group_by(id_setor) %>%
-    mutate(sum = sum(pop_prop_grade)) %>%
+    mutate(sum_pop = sum(pop_total)) %>%
     ungroup() %>%
-    mutate(prop_pop_grade_v1 =  prop_pop_grade/sum)
-  
-  
-  # distribuir moradores e renda proporcionalmente a area
-  ui_fim <- ui %>%
-    group_by(id_grade) %>%
-    summarise(renda_prop = sum(renda_total * prop_area * prop_pop_grade_v1, na.rm = TRUE)) %>%
-    mutate(renda_capta_prop = renda_prop / pop_total)
+    # Calcular a populacao proporcional de cada pedaco dentro do setor
+    mutate(pop_prop_grade =  pop_total/sum_pop) %>%
+    mutate(pop_prop_grade = ifelse(is.nan(pop_prop_grade), 0, pop_prop_grade))
     
   
+  # distribuir renda proporcionalmente a area e populacao do setor
+  ui_tipo1_fim <- ui_tipo1_v2 %>%
+    mutate(renda = renda_total * area_prop_setor * pop_prop_grade) %>%
+    mutate(renda_capta_prop = renda / pop_total %>% as.numeric) %>%
+    select(id_grade, pop_total, renda_total, renda, renda_capta_prop)
   
+  
+  # Para as quadriculas que sao divididas por um setor (tipo 2):
+  `%nin%` = Negate(`%in%`)
+  
+  ui_tipo2 <- grade %>%
+    filter(id_grade %nin% que$id_grade)
+  
+  ui_tipo2_v2 <- ui_tipo2 %>%
+    st_intersection(setor) %>%
+    # tip from https://rpubs.com/rural_gis/255550
+    # Calcular a area de cada pedaco
+    mutate(area_pedaco = st_area(.)) %>%
+    # Calcular a proporcao de cada setor que esta naquele pedaco (essa sera a area a ponderar pela renda)
+    mutate(area_prop_setor = area_pedaco/area_setor)
+    # # Calcular a proporcao de cada grade que esta naquele pedacao
+    # mutate(area_prop_grade =  area_pedaco/area_grade) %>%
+    # # Calcular a quantidade de populacao em cada pedaco (baseado na grade)
+    # mutate(pop_prop_grade = pop_total * area_prop_grade) %>%
+    # # Calcular a proporcao de populacao de cada grade que esta dentro do setor
+    # group_by(id_setor) %>%
+    # mutate(sum_pop = sum(pop_total)) %>%
+    # ungroup() %>%
+    # # Calcular a populacao proporcional de cada pedaco dentro do setor
+    # mutate(pop_prop_grade =  pop_total/sum_pop) %>%
+    # mutate(pop_prop_grade = ifelse(is.nan(pop_prop_grade), 0, pop_prop_grade))
+  
+  
+  # distribuir renda proporcionalmente a area e populacao do setor
+  ui_tipo2_fim <- ui_tipo2_v2 %>%
+    group_by(id_grade, pop_total) %>%
+    summarise(renda = sum(renda_total * area_prop_setor, na.rm = TRUE)) %>%
+    mutate(renda_capta_prop = renda_prop / pop_total)
   
 }
 
 
-ui_fim %>% 
-  # filter(id_grade == 717753) %>%
-  mapview(zcol = "renda_prop")
+# TESTAR O MEU RESULTADO!!!
+
+
+# AGORA VAI!!!!!! ---------------------------------------------------------
+
+# DEU CERTO AQUI!! --------------------------------------------------------
+
+ui <- st_intersection(grade, setor) %>%
+  # tip from https://rpubs.com/rural_gis/255550
+  # Calcular a area de cada pedaco
+  mutate(area_pedaco = st_area(.)) %>%
+  # Calcular a proporcao de cada setor que esta naquele pedaco (essa sera a area a ponderar pela renda)
+  mutate(area_prop_setor = area_pedaco/area_setor) %>%
+  # Calcular a proporcao de cada grade que esta naquele pedacao
+  mutate(area_prop_grade =  area_pedaco/area_grade) %>%
+  # Calcular a quantidade de populacao em cada pedaco (baseado na grade)
+  mutate(pop_prop_grade = pop_total * area_prop_grade) %>%
+  # Calcular a proporcao de populacao de cada grade que esta dentro do setor
+  group_by(id_setor) %>%
+  mutate(sum = sum(pop_prop_grade)) %>%
+  ungroup() %>%
+  # Calcular a populacao proporcional de cada pedaco dentro do setor
+  mutate(pop_prop_grade_no_setor =  pop_prop_grade/sum) %>%
+  # Calcular a renda dentro de cada pedaco
+  mutate(renda_pedaco = renda_total* pop_prop_grade_no_setor)
+
+# Grand Finale
+ui_fim <- ui %>%
+  # Agrupar por grade e somar a renda
+  group_by(id_grade, pop_total) %>%
+  summarise(renda = sum(renda_pedaco, na.rm = TRUE)) %>%
+  mutate(renda_capta = renda/pop_total)
+
+
+
+
+ui_teste <- filter(ui, id_grade == 709528)
+ui_fim_teste <- ui_fim %>% filter(id_grade == 709528)
+mapview(ui_teste)
+
+mapview(grade %>% filter(id_grade == 709528))
+mapview(setor %>% filter(id_setor == 93))
+
+
+vai_setor <- setor %>%
+  filter(id_setor %in% c(2656, 2855, 2854))
+
+bbox_setor <- st_bbox(vai_setor)
+
+vai_ui <- st_crop(ui_fim, bbox_setor)
+
+mapview(vai_ui) + mapview(vai_setor)
+
+
+  
+  
+
+
+
--- test/teste_reproducible.R
+++ test/teste_reproducible.R
@@ -14,39 +14,47 @@ setor <- read_rds(setor_path) %>%
 grade <- read_rds(grade_path) %>%
   dplyr::select(id_grade, pop_total = POP)
 
-mapview(setor)
+# mapview(setor)
 
 
 vai_setor <- setor %>%
   filter(id_setor %in% c(2656, 2855, 2854)) %>%
   rename(id_tract = id_setor, tract_incm = renda_total)
 
-mapview(vai_setor)
+# mapview(vai_setor)
 
 bbox_setor <- st_bbox(vai_setor)
 
 vai_grade <- st_crop(grade, bbox_setor) %>%
   rename(id_square = id_grade, square_pop = pop_total)
 
-mapview(vai_grade) + mapview(vai_setor)
+# mapview(vai_grade) + mapview(vai_setor)
 
-# dput
-dput(vai_setor)
-dput(vai_grade)
+# # dput
+# dput(vai_setor)
+# dput(vai_grade)
 
+# mudar nomes
+tract <-  vai_setor
+square <- vai_grade
 
 # exportar
 
-test_setor <- st_write(vai_setor, "test/tracts.shp")
-test_grade <- st_write(vai_grade, "test/squares.shp")
+# test_setor <- st_write(vai_setor, "test/tracts.shp")
+# test_grade <- st_write(vai_grade, "test/squares.shp")
 
-zip()
+save(tract, square, file = "test/shapes.RData")
 
 
 # AGORA VAI ---------------------------------------------------------------
 
 download.file("https://github.com/ipeaGIT/acesso_oport/raw/master/test/test_setor.rds", "tracts.rds")
 download.file("https://github.com/ipeaGIT/acesso_oport/raw/master/test/test_grade.rds", "squares.rds")
+
+download.file("https://github.com/ipeaGIT/acesso_oport/raw/master/test/shapes.RData", "shapes.RData")
+
+load("shapes.RData")
+
 
 # Open census tracts
 census_tracts <- readr::read_rds("tracts.rds")
