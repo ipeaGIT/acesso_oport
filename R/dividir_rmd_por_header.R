412bdffd8724e677cccd0d2c048c419150b7acc4
Kaue <kauebraga90@gmail.com>
2019-05-23 18:27:07 +0000

    add funcao para dividir arquivos rmarkdown em varios arquivos .R de acordo com o header

---
+++ R/dividir_rmd_por_header.R
@@ -0,0 +1,30 @@
+
+# adaptado de https://stackoverflow.com/questions/35855837/with-knitr-preserve-chunk-options-when-purling-chunks-into-separate-files
+# Essa funcao divide um arquivo .Rmd em diversos arquivos .R de acordo com o header numero 2 (##)
+
+# input_file <- "02_agrupamento.Rmd"
+
+dividir_rmd_por_header <- function(input_file) {
+  
+  purled <- knitr::purl(input_file, documentation = 2)    # purl original file; save name to variable
+  
+  # Extrair o capitulo
+  capitulo <- substr(input_file, 1, 2)
+  
+  lines <- readLines(purled)    # read purled file into a character vector of lines
+  starts <- grep("#' ##", lines)    # use grep to find header row indices
+  stops <- c(starts[-1] - 1L, length(lines))   # end row indices
+  # extract chunk names from headers
+  names <- sub("^#' ## (.*)", '\\1', lines[starts])
+  # ajeitar os nomes
+  names <- janitor::make_clean_names(names)
+  # names <- ifelse(names == lines[starts], '', paste0('_', names)) # clean if no chunk name
+  # make nice file names with chunk number and name (if exists)
+  file_names <- paste0(capitulo, ".", seq_along(starts), "-", names, '.R')
+  for(chunk in seq_along(starts)){    # loop over header rows
+    # save the lines in the chunk to a file
+    writeLines(lines[starts[chunk]:stops[chunk]], con = file_names[chunk])
+  }
+  unlink(purled)    # delete purled file of entire document
+  
+}
