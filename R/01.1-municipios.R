01a0b24e595c6841fa2dca397968f7043e250cb9
rafap_IpeaDesktop <rafa.pereira.br@gmail.com>
2019-08-19 18:56:42 +0000

    atualiza setores censitarios a partir de municipios/geobr

--- R/01.1-municipios.R
+++ R/01.1-municipios.R
@@ -1,20 +1,20 @@
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-###### Download de shape file de municipios e setores censitarios dos  municipios incluidos no projeto
-
-# carregar bibliotecas
-source('./R/fun/setup.R')
+  ###### 0.1.1 Download de shape file de municipios e setores censitarios dos  municipios incluidos no projeto
+  
+  # carregar bibliotecas
+  source('./R/fun/setup.R')
 
 
 ## 1. lista de municipios no projeto
 list_muni_codes <- c( 2304400 # Fortaleza
-                    , 3550308 # Sao Paulo
-                    , 3304557 # Rio de Janeiro
-                    , 4106902 # Curitiba
-                    , 4314902 # Porto Alegre
-                    , 3106200 # Belo Horizonte
-                    , 2211001 # Teresina
-                    )
+                      , 3550308 # Sao Paulo
+                      , 3304557 # Rio de Janeiro
+                      , 4106902 # Curitiba
+                      , 4314902 # Porto Alegre
+                      , 3106200 # Belo Horizonte
+                      , 2211001 # Teresina
+)
 
 
 ## lista de estados com municipios no projeto
@@ -25,12 +25,12 @@ list_muni_codes <- c( 2304400 # Fortaleza
 download_muni_setores <- function(i){
   
   sigla <-  ifelse(i== 2304400, 'for',
-            ifelse(i== 3550308, 'sao',
-            ifelse(i== 3304557, 'rio',
-            ifelse(i== 4106902, 'cur',
-            ifelse(i== 4314902, 'por',
-            ifelse(i== 3106200, 'bel',
-            ifelse(i== 2211001, 'ter', NA)))))))
+                   ifelse(i== 3550308, 'sao',
+                          ifelse(i== 3304557, 'rio',
+                                 ifelse(i== 4106902, 'cur',
+                                        ifelse(i== 4314902, 'por',
+                                               ifelse(i== 3106200, 'bel',
+                                                      ifelse(i== 2211001, 'ter', NA)))))))
   
   
   # criar pasta do municipios
@@ -49,7 +49,7 @@ download_muni_setores <- function(i){
   # salvar setores censitarios
   write_rds(ct_sf, paste0("../data-raw/setores_censitarios/", sigla,"/setores_", sigla,".rds"))
 }
-  
-  
+
+
 # 3. Aplica funcao
 lapply(X=list_muni_codes, FUN=download_muni_setores)
--- R/01.2-setores_censitarios.R
+++ R/01.2-setores_censitarios.R
@@ -1,212 +1,62 @@
-#' ## Setores censitários
-#' 
-#' 
-## ----setores_shapes------------------------------------------------------
-
-# ajeitar os setores
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###### 0.1.2 Seleciona e agrega microdados dos setores censitarios
 
 
-arquivos <- dir("../data-raw/municipios", full.names = T, pattern = "*censitarios.zip$", recursive = T)
-
-out_dir <- paste0("../data-raw/municipios/", str_sub(arquivos, -26, -25))
-
-walk2(arquivos, out_dir, ~unzip(zipfile = .x, exdir = .y))
-
-# # criar pastas
-# walk(str_sub(arquivos, -17, -16), ~dir.create(paste0("../data/municipios/", .)))
-
-# nome dos arquivos .shp para abrir
-arquivos_shp <- dir("../data-raw/municipios", full.names = T, pattern = "^\\d{2}SEE.+.shp$", recursive = T)
-
-# shp <- arquivos_shp[1]
-
-# funcao
-
-shp_to_rds <- function(shp) {
-  
-  shp_files <- st_read(shp, crs = 4326, options = "ENCODING=WINDOWS-1252") %>%
-    dplyr::select(cod_setor = CD_GEOCODI, muni = NM_MUNICIP) %>%
-    mutate(cod_setor = as.character(cod_setor)) %>%
-    mutate(cod_setor = as.numeric(cod_setor))
-  
-  uf <- gsub(".+/(\\D{2})/.+", "\\1", shp)
-  
-  out_dir <- paste0("../data/setores/setores_", uf, ".rds")
-  
-  write_rds(shp_files, out_dir)
-  
-}
+# carregar bibliotecas
+source('./R/fun/setup.R')
 
 
-walk(arquivos_shp, shp_to_rds)
+# Cria data.frame com municipios do projeto
+munis_df <- data.frame( code_muni= c(2304400, 3550308, 3304557, 4106902, 4314902, 3106200, 2211001),
+                           abrev_muni=c('for', 'sao', 'rio', 'cur', 'por', 'bel', 'ter'),
+                           name_muni=c('Fortaleza', 'Sao Paulo', 'Rio de Janeiro', 'Curitiba', 'Porto Alegre', 'Belo Horizonte', 'Teresina'))
 
 
+### 1. Carrega micro dados dos setores censitarios --------------------------------------------------
 
-#' 
-#' 
-## ----setores_var---------------------------------------------------------
-
-setores1 <- fread("../data-raw/setores_censitarios/dados_censo2010A.csv")
-
+## Leitura dos dados
+setores1 <- data.table::fread("../data-raw/setores_censitarios/dados_censo2010A.csv", select= c('Cod_UF', 'Cod_municipio', 'Cod_setor', 'DomRend_V003', 'Dom2_V002'))
 names(setores1)
 
+# filtra apenas municipio do projeto
+setores1 <- setores1[Cod_municipio %in% munis_df$code_muni,]
 
+
+## Renomeia variaveis
 # Renda 6.19 - variavel escolhida: V003 = Total do rendimento nominal mensal dos domicílios particulares permanentes
 setores_renda <-  setores1 %>% 
-  dplyr::select(cod_uf = Cod_UF, cod_muni = Cod_municipio, cod_setor = Cod_setor, renda_total = DomRend_V003)
+  dplyr::select(cod_uf = Cod_UF, cod_muni = Cod_municipio, cod_setor = Cod_setor, renda_total = DomRend_V003, moradores_total = Dom2_V002)
+
+# Criar variavel de renda domicilias per capita de cada setor censitario
+setDT(setores_renda)[, renda_per_capta := renda_total / moradores_total]
+setores_renda[, cod_setor := as.character(cod_setor)]
+
+
+
+
+### 2. Merge dos dados de renda com shapes dos setores censitarios --------------------------------------------------
+
+# funcao para fazer merge dos dados e salve arquivos na pata 'data'
+merge_renda_setores <- function(sigla){
   
-# Moradores 6.3 - variavel escolhida: V002 = Moradores em domicílios particulares permanentes
-setores_moradores <- setores1 %>% 
-  dplyr::select(cod_setor = Cod_setor, moradores_total = Dom2_V002)
-
-# juntar
-
-setores_total <- setores_renda %>%
-  left_join(setores_moradores, by = "cod_setor") %>%
-  mutate(renda_per_capta = renda_total / moradores_total) %>%
-  mutate(cod_setor = as.numeric(cod_setor))
-
-# write_rds(setores_total, "../data/renda_por_setor/renda_por_setor.rds")
-
-
-#' 
-#' 
-## ----setores_juncao------------------------------------------------------
-
-setores_total <- read_rds("../data/renda_por_setor/renda_por_setor.rds")
-
-# dividir por uf
-
-ufs <- tibble::tribble(
-  ~cod_uf,                     ~nome_uf,  ~uf,
-   11,              "Rondônia", "RO",
-   12,                  "Acre", "AC",
-   13,              "Amazonas", "AM",
-   14,               "Roraima", "RR",
-   15,                  "Pará", "PA",
-   16,                 "Amapá", "AP",
-   17,             "Tocantins", "TO",
-   21,              "Maranhão", "MA",
-   22,                 "Piauí", "PI",
-   23,                 "Ceará", "CE",
-   24,   "Rio Grande do Norte", "RN",
-   25,               "Paraíba", "PB",
-   26,            "Pernambuco", "PE",
-   27,               "Alagoas", "AL",
-   28,               "Sergipe", "SE",
-   29,                 "Bahia", "BA",
-   31,          "Minas Gerais", "MG",
-   32,        "Espírito Santo", "ES",
-   33,        "Rio de Janeiro", "RJ",
-   35,             "São Paulo", "SP",
-   41,                "Paraná", "PR",
-   42,        "Santa Catarina", "SC",
-   43, "Rio Grande do Sul (*)", "RS",
-   50,    "Mato Grosso do Sul", "MS",
-   51,           "Mato Grosso", "MT",
-   52,                 "Goiás", "GO",
-   53,      "Distrito Federal", "DF"
-  )
-
-# adicionar coluna para uf
-
-setores_total_v1 <- setores_total %>%
-  # Join com a tabela com o codigo das ufs
-  left_join(ufs, by = "cod_uf") %>%
-  # Transformar em minusculo
-  mutate(uf = tolower(uf)) %>%
-  # Ordernar por uf
-  arrange(uf) %>%
-  # Criar uma lista com um data.frame por UF
-  split(.$uf)
-
-# abrir os shapes dos setores por uf
-
-files <- dir("../data/setores/", full.names = TRUE)
-
-# Abrir em forma de uma lista com um data.frame para cada UF
-setores_shapes <- map(files, read_rds)
-
-# Funcao para criar arquivo agregada (com as variaveis de renda e populacao) para uf
-
-agregar_setores <- function(setores_variaveis1, setores_shapes1) {
+#  sigla <- "for"
   
-  # Extrair a uf do data.frame em questao
-  uf <- unique(setores_variaveis1$uf)[1]
+  # codigo do municipios
+  code_muni <- subset(munis_df, abrev_muni==i )$code_muni
   
-  # Join os shapes com o data.frame das variaveis
-  setores_fim <- setores_shapes1 %>%
-    left_join(setores_variaveis1, by = "cod_setor")
+  # subset dados dos setores
+  dados <- subset(setores_renda, cod_muni == code_muni)
   
+  # leitura do shape dos setores
+  sf <- read_rds( paste0("../data-raw/setores_censitarios/", sigla,"/setores_", sigla,".rds") )
+  
+  # merge
+  sf2 <- left_join(sf, dados, c('code_tract'='cod_setor'))
+    
   # salvar
-  dir_out <- sprintf("../data/setores_agregados_uf/setores_agregados_%s.rds", uf)
-  
-  write_rds(setores_fim, dir_out)
-  
-  
-}
-
-# aplicar
-
-walk2(setores_total_v1, setores_shapes, agregar_setores)
+  write_rds(sf2,  paste0("../data/setores_agregados/setores_agregados_", sigla,".rds"))
+  }
 
 
-
-#' 
-#' 
-## ----setores_por_municipio-----------------------------------------------
-
-# # nome do municipio completo, minusculo, sem acentos
-# municipio_logname <- "brasilia"
-# uf <- "ce"
-
-setores_por_municipio <- function(municipio_logname, uf) {
-  
-  
-  # Abrir setores da uf
-  path_setor_uf <- sprintf("../data/setores_agregados_uf/setores_agregados_%s.rds", uf)
-  setor_uf <- read_rds(path_setor_uf) %>%
-    # transformar para minusculo o nome do municipio
-    mutate(muni = tolower(muni)) %>%
-    # tirar acentos do nome do municipio
-    mutate(muni = rm_accent(muni))
-  
-  # # Abrir tabela com as siglas dos municipios
-  # tabela_muni <- read_delim("../data-raw/tabela_muni_codigos_2010.csv", delim = ";", skip = 2, 
-  #                           locale = locale(encoding = 'WINDOWS-1252')) %>%
-  #   select(municipio, nome_municipio) %>%
-  #   # Mudar para minusculo
-  #   mutate(nome_municipio1 = tolower(nome_municipio)) %>%
-  #   # Tirar acentos do nome do municipio
-  #   # mutate(nome_municipio1 = rm_accent(nome_municipio1)) %>%
-  #   mutate(nome_municipio1 = trimws(nome_municipio1))
-  #   # # Determinar a sigla (tres primeiras letras)
-  #   # mutate(nome_municipio1 = substr(nome_municipio1, 1, 3))
-  # 
-  # # Fazer juncao
-  # muni_desejado <- tabela_muni %>%
-  #   filter(nome_municipio1 == municipio_logname)
-  
-  setor_municipio <- setor_uf %>%
-    filter(muni == municipio_logname)
-  
-  # Salvar
-  muni_shortname <- substr(municipio_logname, 1, 3)
-  path_out <- sprintf("../data/setores_agregados/setores_agregados_%s.rds", muni_shortname)
-  write_rds(setor_municipio, path_out)
-  
-}
-
-# Aplicar funcao
-
-municipio_logname <- c("fortaleza", "rio de janeiro", "belo horizonte", "teresina", "porto alegre", "sao paulo", "curitiba")
-ufs <- c("ce", "rj", "mg", "pi", "rs", "sp", "pr")
-
-walk2(municipio_logname, ufs, setores_por_municipio)
-
-# para brasilia
-source("R/fun/setup.R")
-setores_por_municipio("brasilia", "df")
-
-
-#' 
+# aplicar funcao
+walk(munis_df$abrev_muni, merge_renda_setores)
