a8ae6629e0527d7c6040145f18413ad609200164
rafapereirabr <rafa.pereira.br@gmail.com>
2019-09-18 10:01:54 -0300

    one muni missing in fig4

--- R/criar_mapas_webinar.R
+++ R/criar_mapas_webinar.R
@@ -318,6 +318,11 @@ df4 <- acess_walk[, .(Total = weighted.mean(x = TMIEM[which(pop_total>0)], w = p
                       Q5 = weighted.mean(TMIEM[which(quintil==5)], w = pop_total[which(quintil==5)], na.rm=T)), by=city]
 
 
+unique(acess_walk$muni)
+unique(df4$city)
+
+
+
 # ajeitar nome das cidade
   df4 %<>%
     mutate(city = ifelse(city == "bel", "bho", ifelse(city == "sao", "spo", ifelse(city == "por", "poa", city)))) %>%
