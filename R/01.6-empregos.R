2d066dc3d438576016353dabb0907ad7a09dabce
Kauê Braga <kauebraga90@gmail.com>
2019-10-09 18:41:58 -0300

    add filtro por natureza juridica e corrige cnae para 2.0

--- R/01.6-empregos.R
+++ R/01.6-empregos.R
@@ -5,7 +5,7 @@
   
   
 # carregar bibliotecas
-  source('./R/fun/setup.R')
+source('./R/fun/setup.R')
 
 
  
@@ -13,36 +13,49 @@
 
 
 # Leitura dos dados da RAIS pessoas com colunas que vamos usar
-rais <- data.table::fread("../data-raw/rais/brasil2017.csv",
-              select = c("id_estab", "grau_instr", "emp_31dez", 'clas_cnae10', 'uf', 'codemun'), colClasses='character'
-               #,nrows = 1000
+rais <- data.table::fread("../data-raw/rais/brasil2017.csv"
+              , select = c("id_estab", "grau_instr", "emp_31dez", 'clas_cnae20', 'uf', 'codemun', 'nat_jur2016')
+              , colClasses='character'
+               # ,nrows = 1000
               )
 
 
+# deletar todas as empresas que tenham 'publico' na sua natureza juridica (ver ../data-raw/rais/ManualRAIS2018.pdf)
+# todos que comecam com o numeral 1 sao de administracao publica
+# a entidade 2011 eh empresa publica
 
-# Salvar em formato rds para formato rapido
-write_rds(rais, "../data/rais/rais_2017_ind.rds")
 
-
-# Abrir RAIS  em formato rapido rds
-rais <- read_rds("../data/rais/rais_2017_ind.rds")
+rais <- rais[substr(nat_jur2016, 1, 1) != 1]
+rais <- rais[nat_jur2016 != "2011"]
 
 
 # selecionar so vinculos ativos
 rais <- rais[emp_31dez == 1]
 
+# Salvar em formato rds para formato rapido
+write_rds(rais, "../data/rais/rais_2017_ind.rds")
+
+
+
+
+# ABRIR RAIS ----------------------------------------------------------------------------------
+
+# Abrir RAIS  em formato rapido rds
+rais <- read_rds("../data/rais/rais_2017_ind.rds")
+
+
 
 # Categorizar trabalhadores por grau de instrucao
-rais_cats <- rais[, instrucao := ifelse(grau_instr %in% c(1:6), "baixo",                                    # menor do que ensino medio (inclui ensino medio incompleto)
-                                         ifelse(grau_instr %in% c(7, 8), "medio",                           # ensino medio
-                                                ifelse(grau_instr %in% c(9, 10, 11), "alto", grau_instr)))] # ensino superior
+rais_cats <- rais[, instrucao := fifelse(grau_instr %in% c(1:6), "baixo",                                    # menor do que ensino medio (inclui ensino medio incompleto)
+                                         fifelse(grau_instr %in% c(7, 8), "medio",                           # ensino medio
+                                                fifelse(grau_instr %in% c(9, 10, 11), "alto", grau_instr)))] # ensino superior
 
 
 # Calcula quantidade de vinculo por grau de instrucao em cada estabelecimento
-rais_fim <- rais[, .(vinculos = .N), by = .(id_estab, clas_cnae10, instrucao)]
+rais_fim <- rais[, .(vinculos = .N), by = .(id_estab, clas_cnae20, instrucao)]
 
 # soma quantidade total de empregados em cada empresa
-rais_fim <- rais_fim[, total := sum(vinculos), by = .(id_estab, clas_cnae10)]
+rais_fim <- rais_fim[, total := sum(vinculos), by = .(id_estab, clas_cnae20)]
 head(rais_fim)
   
 # Reshape da base de formato long para wide
@@ -53,9 +66,10 @@ head(rais_fim_wide)
 data.table::fwrite(rais_fim_wide, "../data/rais/rais_2017_vin_instrucao.csv")
 
 
+
+
+
 ### Limpeza de outliers -----------------------------------------------------
-
-
 
 # Abrir
 rais <- fread("../data/rais/rais_2017_vin_instrucao.csv")
@@ -63,16 +77,17 @@ rais <- fread("../data/rais/rais_2017_vin_instrucao.csv")
 # Limpeza de estabelecimentos com total de empregos outliers _ do codigo do Bruno ------------------
 
 # Extrair o cnae do setor
-rais <- setDT(rais)[, cnae.setor := substr(clas_cnae10, 1, 2)]
+rais <- setDT(rais)[, cnae.setor := substr(clas_cnae20, 1, 2)]
 
 # Extrair os setores desejados
-  # 40 eletricidade, gas e agua quente
-  # 41 captacao, tratamento e distribuicao de agua
-  # 60 transporte terrestre
-  # 62 transporte aereo
-  # 74 servicoes prestados principalmente a empresas
-  # 90 limpeza urbana, esgoto e atividades relacionadas
-  rais.problema <- setDT(rais)[cnae.setor %in% c("40","41","60","62","74","90")]
+  # 35 eletricidade, gas e agua quente OK
+  # 36 captacao, tratamento e distribuicao de agua OK
+  # 49 transporte terrestre OK
+  # 51 transporte aereo OK
+  # 82 servicoes prestados principalmente a empresas
+  # 38 limpeza urbana, esgoto e atividades relacionadas (COLETA, TRATAMENTO E DISPOSIÇÃO DE RESÍDUOS; RECUPERAÇÃO DE MATERIAIS 2.0)
+
+  rais.problema <- setDT(rais)[cnae.setor %in% c("35","36","49","51","82","38")]
 
 
 # Funcoes para identificar ouliet
@@ -120,7 +135,7 @@ names(geral2)<-c("cnae.setor","q","freq","desviointerq","fator","corte","outlier
 rais$total_corrigido <- rais$total
 
 #zerando empregos de administracao publica
-rais$total_corrigido<-ifelse(rais$cnae.setor=="75",0,rais$total_corrigido)
+rais$total_corrigido<-ifelse(rais$cnae.setor=="84",0,rais$total_corrigido)
 
 #tabela com valor de corte por setor
 geral3<-geral2[,c(1,6)]
@@ -131,8 +146,7 @@ rais <- merge(rais, geral3,
               all.x = TRUE)
 
 #substituindo valores maiores que o corte pelo valor de corte
-rais$total_corrigido<-ifelse(rais$cnae.setor %in% c("40","41","60","62","74","90") & rais$total_corrigido>rais$corte,rais$corte,rais$total_corrigido)
-
+rais$total_corrigido<-ifelse(rais$cnae.setor %in% c("35","36","49","51","82","38") & rais$total_corrigido>rais$corte,rais$corte,rais$total_corrigido)
 
 
 
@@ -182,10 +196,9 @@ rais[, c('corte', 'prop_baixo', 'prop_medio',  'prop_alto') := NULL]
 
 # leitura da RAIS geocodificada
 rais_geo <- fread("../data-raw/rais/rais_2017_raw.csv", 
-                  colClasses='character', 
                   select = c("id_estab", 'codemun', "lat", "lon"))
 
-rais_geo[, id_estab := as.numeric(id_estab)]
+# rais_geo[, id_estab := as.numeric(id_estab)]
 head(rais_geo)
 
 # corrigir coordenadas
@@ -199,10 +212,13 @@ rais_geo[, ':='(lon = as.numeric(lon),
 rais[rais_geo, on = 'id_estab', c('lat', 'lon', 'codemun') := list(i.lat, i.lon, i.codemun)]
 head(rais)
 
+# qual a porcentagem de coordenadas NA?
+# 13%
 rais[is.na(lat), sum(total_corrigido)] / sum(rais$total_corrigido)
 
 # Salvar
 write_rds(rais, "../data/rais/rais_2017_corrigido.rds")
+
 
 
 # Salvar base somente com municipios do projeto ----------------------------------------------------
