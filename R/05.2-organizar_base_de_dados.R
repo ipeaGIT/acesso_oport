1c7874176620b170b2912c3c72b8b145a1c45639
rafapereirabr <rafa.pereira.br@gmail.com>
2019-11-27 16:47:26 -0300

    corrections to data output acess_oport_2019

--- R/05.2-organizar_base_de_dados.R
+++ R/05.2-organizar_base_de_dados.R
@@ -22,39 +22,57 @@ source('./R/fun/setup.R')
 # dados hex agregados
 hex_agreg <- lapply(dir("../data/hex_agregados/", full.names = TRUE, pattern = "09"), read_rds) %>% rbindlist(fill = TRUE)
 
+# incorporar nome da cidade e codigo do municipio
+hex_agreg <- hex_agreg %>% left_join(munis_df %>% select(cod_muni = code_muni, abrev_muni, nome_muni = name_muni), 
+                               by = c("muni" = "abrev_muni"))
+
+
 # dados acessibilidade
 acess <- lapply(dir("../data/output_access/", full.names = TRUE), read_rds) %>% rbindlist(fill = TRUE)
 setDT(acess)
 setnames(acess, 'origin', 'id_hex' )
 
-# join data sets
-hex_dt <- left_join(acess, hex_agreg[, -"geometry", with =F], by=c("id_hex", "quintil", "decil"))
-setDT(hex_dt)   
 
-# incorporar nome da cidade e codigo do municipio
-hex_dt <- hex_dt %>% left_join(munis_df %>% select(cod_muni = code_muni, abrev_muni, nome_muni = name_muni), 
-                               by = c("city" = "abrev_muni"))
+# join data sets
+hex_dt <- left_join(hex_agreg, acess[, -"geometry", with =F], by=c("id_hex", "quintil", "decil"))
+setDT(hex_dt)  
+head(hex_dt)
 
 
 # 2) Organizar a nomeclatura das variaveis ---------------------------
 
 hex_dt_fim <- hex_dt %>%
-  select(sigla_muni = city, nome_muni, cod_muni, id_hex, 
+  select(sigla_muni = muni, nome_muni, cod_muni, id_hex, 
          
          # Selecionar variaveis de populacao
-         P001 = pop_total, P002 = cor_branca, P003 = cor_negra, P004 = cor_indigena, P005 = cor_amarela,
+         P001 = pop_total, 
+         P002 = cor_branca, 
+         P003 = cor_negra, 
+         P004 = cor_indigena, 
+         P005 = cor_amarela,
          
          # Selecionar variveis de renda
-         R001 = renda_capta, R002 = quintil, R003 = decil,
+         R001 = renda_capta, 
+         R002 = quintil, 
+         R003 = decil,
          
-         # Selecionar atividades de trabalho
-         T001 = empregos_total, T002 = empregos_baixa, T003 = empregos_media, T004 = empregos_alta,
+         # # Selecionar atividades de trabalho
+         # T001 = empregos_total, 
+         # T002 = empregos_baixa, 
+         # T003 = empregos_media, 
+         # T004 = empregos_alta,
          
          # Selecionar atividades de educacao
-         E001 = edu_total, E002 = edu_infantil, E003 = edu_fundamental, E004 = edu_medio,
+         E001 = edu_total, 
+         E002 = edu_infantil,
+         E003 = edu_fundamental, 
+         E004 = edu_medio,
          
          # Selecionar atividades de saude
-         S001 = saude_total, S002 = saude_baixa, S003 = saude_media, S004 = saude_alta,
+         S001 = saude_total, 
+         S002 = saude_baixa, 
+         S003 = saude_media, 
+         S004 = saude_alta,
          
          # Selecionar variaveis de acessibilidade
          modo = mode, pico, 
@@ -62,13 +80,16 @@ hex_dt_fim <- hex_dt %>%
          # Selecionar a geometria
          geometry) %>%
   
-  # transformar Infs para 0 na renda per capta
-  mutate(R001 = ifelse(is.infinite(R001), 0, R001)) %>%
+  # transformar Infs para NA na renda per capta (renda Inf ocorre quando nao há pop no hexagono)
+  mutate(R001 = ifelse(P001==0, NA , R001)) %>%
+  mutate(R002 = ifelse(P001==0, NA , R002)) %>%
+  mutate(R003 = ifelse(P001==0, NA , R003)) %>%
+  
   
   # arredondar variaveis
-  # arredondar renda per capta para 0 casas decimais
+  # arredondar renda per capta para 1 casas decimais
   mutate(R001 = round(R001, 1)) %>%
-  # arredondar acessibilidad para tres casas decimais
+  # arredondar acessibilidade para 4 casas decimais
   mutate_at(vars(matches("CMA|TMI")), round, digits = 4) %>%
   
   # renomear os modos
@@ -78,10 +99,10 @@ hex_dt_fim <- hex_dt %>%
     modo == "walk" ~ "caminhada"
   )) %>%
   
-  # truncar os valores de TMI quando eh infinito
-  # para caminhada: 60 minutos;  para bicicleta de transporte publico: 120 minutos
-  mutate_at(vars(matches("TMI")), list(~ ifelse(is.infinite(.) & modo == "caminhada", 60, 
-                                             ifelse(is.infinite(.) & modo %in% c("tp", "bicicleta"), 120, .)))) %>%
+  # # truncar os valores de TMI quando eh infinito
+  # # para caminhada: 60 minutos;  para bicicleta de transporte publico: 120 minutos
+  # mutate_at(vars(matches("TMI")), list(~ ifelse(is.infinite(.) & modo == "caminhada", 60, 
+  #                                            ifelse(is.infinite(.) & modo %in% c("tp", "bicicleta"), 120, .)))) %>%
   
   # transformar para sf
   st_sf()
@@ -92,16 +113,32 @@ names(hex_dt_fim)
 
 
 
+
+
 # 3) Exportar ---------------------------
 
-# salvar dados em shapefile
-st_write(hex_dt_fim, "../data/output_base_final/shp/acess_oport_2019.shp")
-# zip
-zip::zipr(zipfile = "../data/output_base_final/acess_oport_2019.zip", 
-          files = dir("../data/output_base_final/shp", pattern = "*.shp|*.shx|*.dbf|.*prj", full.names = TRUE))
-
-
-# st_write(hex_dt_fim, "acess_oport_2019.geodb")
-
 # salvar rds
-write_rds(hex_dt_fim, "../data/output_base_final/acess_oport_2019.rds", )
+write_rds(hex_dt_fim, "../data/output_base_final/acess_oport_2019.rds", compress = "gz")
+
+# salvar dados em geopakage
+st_write(hex_dt_fim, "../data/output_base_final/acess_oport_2019.gpkg")
+
+  # zip
+  zip::zipr(zipfile = "../data/output_base_final/acess_oport_2019.zip", 
+            files = dir("../data/output_base_final/", pattern = "*.gpkg", full.names = TRUE))
+
+  
+  
+  
+  
+  
+  
+aaa <- subset(hex_dt_fim, sigla_muni %in% c('bel', 'poa'))  
+  
+  
+  
+  write_rds(aaa, "../data/output_base_final/acess_oport_2019_test.rds", compress = "gz")
+  
+  
+  
+  
