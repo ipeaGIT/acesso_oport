026c8529aafd3671a76ac4854298efbda6072fa4
Kauê Braga <kauebraga90@gmail.com>
2019-12-20 17:47:40 -0300

    update data structure

--- apps/app_bootstrap_style/cleaning.R
+++ apps/app_bootstrap_style/cleaning.R
@@ -1,8 +1,3 @@
-library(sf)
-library(readr)
-library(dplyr)
-library(mapview)
-
 source("../../R/fun/setup.R")
 
 # abrir base final
@@ -17,11 +12,11 @@ acess_tp <- acess %>% filter(modo == "tp")
 acess_tp <- acess_tp %>% filter(pico == 1)
 
 # filter columns
-acess_tp <- acess_tp %>% dplyr::select(nome_muni, P001, matches("30|60|90|120"), starts_with("TMI"))
+acess_tp <- acess_tp %>% dplyr::select(id_hex, sigla_muni, P001, matches("30|60|90|120"), starts_with("TMI"))
 
 # separate between indicators
-acess_tp_cum <- acess_tp %>% dplyr::select(nome_muni, P001, matches("30|60|90|120"))
-acess_tp_min <- acess_tp %>% dplyr::select(nome_muni, P001, starts_with("TMI"))
+acess_tp_cum <- acess_tp %>% dplyr::select(id_hex, sigla_muni, P001, matches("30|60|90|120"))
+acess_tp_min <- acess_tp %>% dplyr::select(id_hex, sigla_muni, P001, starts_with("TMI"))
 
 # from wide to long cum
 acess_tp_cum_long <- acess_tp_cum %>%
@@ -46,38 +41,60 @@ acess_tp_min_long <- acess_tp_min %>%
 
 
 # salvar
-write_rds(acess_tp_cum_long,   # tirar sao paulo e rio para teste
-          # filter(nome_muni %nin% c("São Paulo", "Rio de Janeiro"))
-          "acess_tp_cum_app.rds") 
-
-write_rds(acess_tp_min_long,  # tirar sao paulo e rio para teste
-            # filter(nome_muni %nin% c("São Paulo", "Rio de Janeiro")), 
-          "acess_tp_min_app.rds") 
+# write_rds(acess_tp_cum_long,   # tirar sao paulo e rio para teste
+#           # filter(nome_muni %nin% c("São Paulo", "Rio de Janeiro"))
+#           "acess_tp_cum_app.rds") 
+# 
+# write_rds(acess_tp_min_long,  # tirar sao paulo e rio para teste
+#             # filter(nome_muni %nin% c("São Paulo", "Rio de Janeiro")), 
+#           "acess_tp_min_app.rds") 
 
 # salvar sem geometria
-write_rds(acess_tp_cum_long %>% st_set_geometry(NULL),   # tirar sao paulo e rio para teste
+write_rds(acess_tp_cum_long %>% st_set_geometry(NULL) %>% setDT(),   # tirar sao paulo e rio para teste
           # filter(nome_muni %nin% c("São Paulo", "Rio de Janeiro")), 
-          "acess_tp_cum_app_sgeo.rds") 
+          "data/acess_tp_cum_app_sgeo.rds") 
 
-write_rds(acess_tp_min_long %>%  st_set_geometry(NULL),   # tirar sao paulo e rio para teste
+write_rds(acess_tp_min_long %>%  st_set_geometry(NULL) %>% setDT(),   # tirar sao paulo e rio para teste
           # filter(nome_muni %nin% c("São Paulo", "Rio de Janeiro")), 
-          "acess_tp_min_app_sgeo.rds") 
+          "data/acess_tp_min_app_sgeo.rds") 
 
 
 # geometrias ----------------------------------------------------------------------------------
 
 # dados hex agregados
-hex_agreg <- lapply(dir("../data/hex_agregados/", full.names = TRUE, pattern = "09"), read_rds) %>% rbindlist(fill = TRUE)
+hex_agreg <- lapply(dir("../../../data/hex_agregados/", full.names = TRUE, pattern = "09"), read_rds) %>% rbindlist(fill = TRUE)
 
 # pegar pontos nao-vazios
-points <- read_csv("../otp/")
+points <- lapply(dir("../../../otp/points/", pattern = "*09.csv", full.names = TRUE), read_csv) %>% rbindlist()
+
+# filtrar so nao-vazios nos hex
+hex_n_vazios <- hex_agreg %>%
+  filter(id_hex %in% points$id_hex) %>%
+  # teste: so cidades de tp
+  filter(muni %in% munis_df[modo == "todos"]$abrev_muni) %>%
+  # selecionar colunas
+  dplyr::select(id_hex, geometry)
 
 
 
-# para abrir
-system.time(read_rds("acess_tp_cum_app.rds")) # 4.3s
+# salvar
+write_rds(hex_n_vazios %>% setDT(), "data/hex_teste.rds")
 
-system.time(read_rds("acess_tp_cum_app_sgeo.rds")) # 1.14s
-            
-            read_r
-            ) # 0.8s
+                 
+                
+
+# TESTES --------------------------------------------------------------------------------------
+
+
+# # para abrir
+# system.time(read_rds("acess_tp_cum_app.rds")) # 36s
+# 
+# 
+# system.time(a <- read_rds("acess_tp_cum_app_sgeo.rds")) # 9.8s 
+#             
+# system.time(b <- read_rds("hex_teste.rds")) # 0.7s
+# 
+# system.time(c <- a[nome_muni == "Fortaleza"]) # 0.01s
+#   
+# system.time(merge(c, b, by = "id_hex", all.x = TRUE) %>% st_sf(crs = 4326)) # 0.24s
+#   
