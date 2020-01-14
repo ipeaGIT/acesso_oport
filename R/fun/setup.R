348bbeb48013af165f705b91b3b73832c2992ec7
Kauê Braga <kauebraga90@gmail.com>
2020-01-14 14:49:22 -0300

    add fun change_remotes to seteup

--- R/fun/setup.R
+++ R/fun/setup.R
@@ -143,3 +143,15 @@ sfc_as_cols <- function(x, names = c("lon","lat")) {
   st_set_geometry(ui, NULL)
 }
 
+
+
+# Function to switch between git remotes in a project
+
+change_remotes <- function(remote, branch = "master") {
+  
+  command <- sprintf("git branch --set-upstream-to=%s/%s", remote, branch)
+  
+  shell(command)
+  
+  
+}
