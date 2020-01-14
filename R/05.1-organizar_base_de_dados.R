bf5b494fc6ff54711bdff092bdc6b47505443794
Kauê Braga <kauebraga90@gmail.com>
2020-01-14 16:17:21 -0300

    fix border problem

--- R/05.1-organizar_base_de_dados.R
+++ R/05.1-organizar_base_de_dados.R
@@ -33,106 +33,117 @@ setDT(acess)
 setnames(acess, 'origin', 'id_hex' )
 
 
-# join data sets
-hex_dt <- left_join(hex_agreg, acess[, -"geometry", with =F], by=c("id_hex", "quintil", "decil"))
-setDT(hex_dt)  
-head(hex_dt)
+# # join data sets
+# hex_dt <- left_join(hex_agreg, acess[, -"geometry", with =F], by=c("id_hex", "quintil", "decil"))
+# setDT(hex_dt)  
+# head(hex_dt)
 
 
 # 2) Organizar a nomeclatura das variaveis ---------------------------
 
-hex_dt_fim <- hex_dt %>%
-  select(sigla_muni = muni, nome_muni, cod_muni, id_hex, 
-         
-         # Selecionar variaveis de populacao
-         P001 = pop_total, 
-         P002 = cor_branca, 
-         P003 = cor_negra, 
-         P004 = cor_indigena, 
-         P005 = cor_amarela,
-         
-         # Selecionar variveis de renda
-         R001 = renda_capta, 
-         R002 = quintil, 
-         R003 = decil,
-         
-         # # Selecionar atividades de trabalho
-         # T001 = empregos_total, 
-         # T002 = empregos_baixa, 
-         # T003 = empregos_media, 
-         # T004 = empregos_alta,
-         
-         # Selecionar atividades de educacao
-         E001 = edu_total, 
-         E002 = edu_infantil,
-         E003 = edu_fundamental, 
-         E004 = edu_medio,
-         
-         # Selecionar atividades de saude
-         S001 = saude_total, 
-         S002 = saude_baixa, 
-         S003 = saude_media, 
-         S004 = saude_alta,
-         
-         # Selecionar variaveis de acessibilidade
-         modo = mode, pico, 
-         starts_with("CMA"), starts_with("TMI"),
-         # Selecionar a geometria
-         geometry) %>%
+# munii <- "for"
+
+join_by_muni <- function(munii) {
   
-  # transformar Infs para NA na renda per capta (renda Inf ocorre quando nao há pop no hexagono)
-  mutate(R001 = ifelse(P001==0, NA , R001)) %>%
-  mutate(R002 = ifelse(P001==0, NA , R002)) %>%
-  mutate(R003 = ifelse(P001==0, NA , R003)) %>%
+  # filter muni
+  hex_agreg_muni <- hex_agreg %>% filter(muni == munii)
+  acess_muni <- acess %>% filter(city == munii)
   
+  # join data sets
+  hex_dt <- left_join(setDT(hex_agreg_muni), setDT(acess_muni)[, -"geometry", with =F], 
+                      by=c("id_hex", "quintil", "decil"))
   
-  # arredondar variaveis
-  # arredondar renda per capta para 1 casas decimais
-  mutate(R001 = round(R001, 1)) %>%
-  # arredondar acessibilidade para 4 casas decimais
-  mutate_at(vars(matches("CMA|TMI")), round, digits = 4) %>%
+  hex_dt_fim <- hex_dt %>%
+    select(sigla_muni = muni, nome_muni, cod_muni, id_hex, 
+           
+           # Selecionar variaveis de populacao
+           P001 = pop_total, 
+           P002 = cor_branca, 
+           P003 = cor_negra, 
+           P004 = cor_indigena, 
+           P005 = cor_amarela,
+           
+           # Selecionar variveis de renda
+           R001 = renda_capta, 
+           R002 = quintil, 
+           R003 = decil,
+           
+           # # Selecionar atividades de trabalho
+           # T001 = empregos_total, 
+           # T002 = empregos_baixa, 
+           # T003 = empregos_media, 
+           # T004 = empregos_alta,
+           
+           # Selecionar atividades de educacao
+           E001 = edu_total, 
+           E002 = edu_infantil,
+           E003 = edu_fundamental, 
+           E004 = edu_medio,
+           
+           # Selecionar atividades de saude
+           S001 = saude_total, 
+           S002 = saude_baixa, 
+           S003 = saude_media, 
+           S004 = saude_alta,
+           
+           # Selecionar variaveis de acessibilidade
+           modo = mode, pico, 
+           starts_with("CMA"), starts_with("TMI"),
+           # Selecionar a geometria
+           geometry) %>%
+    
+    # transformar Infs para NA na renda per capta (renda Inf ocorre quando nao há pop no hexagono)
+    mutate(R001 = ifelse(P001==0, NA , R001)) %>%
+    mutate(R002 = ifelse(P001==0, NA , R002)) %>%
+    mutate(R003 = ifelse(P001==0, NA , R003)) %>%
+    
+    
+    # arredondar variaveis
+    # arredondar renda per capta para 1 casas decimais
+    mutate(R001 = round(R001, 1)) %>%
+    # arredondar acessibilidade para 4 casas decimais
+    mutate_at(vars(matches("CMA|TMI")), round, digits = 4) %>%
+    
+    # renomear os modos
+    mutate(modo = case_when(
+      modo == "transit" ~ "tp",
+      modo == "bike" ~"bicicleta",
+      modo == "walk" ~ "caminhada"
+    )) %>%
+    
+    # filtrar cidades que so tem ativo que porventura tenham tp
+    mutate(ok = ifelse(sigla_muni %in% munis_df[modo == "ativo"]$abrev_muni & modo == "tp", 1, 0)) %>%
+    filter(is.na(ok) | ok == 0) %>%
+    select(-ok) %>%
+    
+    # garantir que so tenha um unico hexagono para cada cidade, modo e pico
+    distinct(id_hex, sigla_muni, modo, pico, .keep_all = TRUE)
+    
+    
+    
+    # # truncar os valores de TMI quando eh infinito
+    # # para caminhada: 60 minutos;  para bicicleta de transporte publico: 120 minutos
+    # mutate_at(vars(matches("TMI")), list(~ ifelse(is.infinite(.) & modo == "caminhada", 60, 
+    #                                            ifelse(is.infinite(.) & modo %in% c("tp", "bicicleta"), 120, .)))) %>%
   
-  # renomear os modos
-  mutate(modo = case_when(
-    modo == "transit" ~ "tp",
-    modo == "bike" ~"bicicleta",
-    modo == "walk" ~ "caminhada"
-  )) %>%
-  
-  # filtrar cidades que so tem ativo que porventura tenham tp
-  mutate(ok = ifelse(sigla_muni %in% munis_df[modo == "ativo"]$abrev_muni & modo == "tp", 1, 0)) %>%
-  filter(is.na(ok) | ok == 0) %>%
-  select(-ok) %>%
-  
-  # garantir que so tenha um unico hexagono para cada cidade, modo e pico
-  distinct(id_hex, sigla_muni, modo, pico, .keep_all = TRUE) %>%
+}
 
 
-  
-  # # truncar os valores de TMI quando eh infinito
-  # # para caminhada: 60 minutos;  para bicicleta de transporte publico: 120 minutos
-  # mutate_at(vars(matches("TMI")), list(~ ifelse(is.infinite(.) & modo == "caminhada", 60, 
-  #                                            ifelse(is.infinite(.) & modo %in% c("tp", "bicicleta"), 120, .)))) %>%
-  
-  # transformar para sf
-  st_sf()
-  
+# aplicar funcao
 
+a <- lapply(munis_df$abrev_muni, join_by_muni) %>% rbindlist()
 
-names(hex_dt_fim)
-
-
-
+a_sf <- st_sf(a, crs = 4326)
 
 
 # 3) Exportar ---------------------------
 
 # salvar rds
-write_rds(hex_dt_fim, "../data/output_base_final/dados2019_AcessOport_v1.0_20200116.rds", compress = "gz")
+write_rds(a_sf, "../data/output_base_final/dados2019_AcessOport_v1.0_20200116.rds", compress = "gz")
 
 
 # salvar dados em geopackage
-st_write(hex_dt_fim, "../data/output_base_final/dados2019_AcessOport_v1.0_20200116.gpkg")
+st_write(a_sf, "../data/output_base_final/dados2019_AcessOport_v1.0_20200116.gpkg")
 
   # zip
   zip::zipr(zipfile = "../data/output_base_final/dados2019_AcessOport_v1.0_20200116.zip", 
@@ -143,7 +154,7 @@ st_write(hex_dt_fim, "../data/output_base_final/dados2019_AcessOport_v1.0_202001
   
 ## exportanto dados para ITDP Mobilidados
 # so modos ativos e acesso a saude e educacao
-atv <- subset(hex_dt_fim, modo %in% c('bicicleta', 'caminhada'))
+atv <- subset(a_sf, modo %in% c('bicicleta', 'caminhada'))
 atv <- atv %>% select(-contains("CMATT"))
 atv <- atv %>% select(-contains("CMATD"))
 atv <- atv %>% select(-contains("CMATQ"))
