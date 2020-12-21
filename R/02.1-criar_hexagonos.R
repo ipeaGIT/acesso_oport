292ad103837625d359d5f4bd5b5106b59ecaae37
Kaue Braga <kauebraga90@gmail.com>
2020-12-21 19:15:41 +0000

    fix hex creation

--- R/02.1-criar_hexagonos.R
+++ R/02.1-criar_hexagonos.R
@@ -1,15 +1,16 @@
 
-  # # adiciona sigla do municipio
-  # hex_grid$muni <- sigla_muni
-  
-  
-  
+# # adiciona sigla do municipio
+# hex_grid$muni <- sigla_muni
+
+
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ###### 0.2.1 Cria grade de haxogonos para os municipios
 
 
 # carregar bibliotecas
 source('./R/fun/setup.R')
+library(h3jsr)
 
 
 
@@ -19,16 +20,19 @@ criar_hexagonos <- function(ano, munis = "all") {
   
   # Select the corerspondent munis_df
   # munis_df <- get(sprintf("munis_df_%s", ano))
-  munis_df <- munis_df_2019
+  # munis_df <- munis_df_2019
+  
+  # sigla_muni <- 'slz'
+  # sigla_muni <- 'poa'
   
   shape_to_hexagon <- function(sigla_muni) {
     
-    code <- filter(munis_df_2019, abrev_muni == sigla_muni)
+    code <- filter(munis_df, abrev_muni == sigla_muni)
     
     code <- code$code_muni
     
-    muni <- geobr::read_municipality(code) %>% 
-      st_transform(crs=4326) %>% # projecao
+    muni <- geobr::read_municipality(code, year = 2019) %>% 
+      st_transform(crs=4326) %>%
       # Buffer para extender a area do municipio e assim evitar que os hexagonos nao considerem areas de borda
       st_buffer(dist = 0.003)
     
@@ -40,28 +44,34 @@ criar_hexagonos <- function(ano, munis = "all") {
     # 07 ~2510 meters
     res_todas <- c(8, 9)
     
+    # resolution <- res_todas[2]
+    
     
     
     make_hex <- function(resolution) {
       
       # get the unique h3 ids of the hexagons intersecting your polygon at a given resolution
-      hex_ids <- h3jsr::polyfill(muni, res = resolution, simple = FALSE)
+      hex_ids <- polyfill(muni, res = resolution, simple = TRUE)
       
       # pass the h3 ids to return the hexagonal grid
-      hex_grid <- unlist(hex_ids$h3_polyfillers) %>% 
-        h3jsr::h3_to_polygon(simple = FALSE) %>%
+      hex_grid <- hex_ids[[1]] %>% 
+        h3_to_polygon(simple = FALSE) %>%
         rename(id_hex = h3_address) %>%
         as_tibble() %>% 
-        mutate(sigla_muni = sigla_muni) %>%
+        mutate(sigla_muni = sigla_muni)
+      
+      # delete possible duplicates
+      hex_grid <- distinct(hex_grid, id_hex, .keep_all = TRUE) %>%
         st_sf()
       
+      # mapview(select(hex_grid, geometry))
       
       # salvar ------------------------------------------------------------------
       
       # rsolution
       if (nchar(resolution) == 1) res_fim <- paste0("0", resolution) else res_fim <- resolution
       
-      dir.create(sprintf("../../data/acesso_oport/hex_municipio/%s", ano))
+      # dir.create(sprintf("../../data/acesso_oport/hex_municipio/%s", ano))
       
       # salvar no disco
       write_rds(hex_grid, 
