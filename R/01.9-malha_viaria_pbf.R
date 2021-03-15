47defe2c2a7344ca3a47d465089e6f80a02b534e
Marcus Saraiva <marcus.saraiva@gmail.com>
2021-03-15 15:56:29 -0300

    Adicionado filtro aos arquivos pbf da malha viária para eliminar conteúdo desnecessário.

--- R/01.9-malha_viaria_pbf.R
+++ R/01.9-malha_viaria_pbf.R
@@ -4,6 +4,9 @@
 #' Esse script extrai a malha viária de cada cidade, de acordo com a bounding box
 #' do arquivo raster de topografia. 
 #' 
+#' Instruções sobre como fazer isso foram retiradas de 
+#' https://docs.analysis.conveyal.com/prepare-inputs#cropping
+#' 
 #' Primeiro, é necessário baixar o arquivo PBF do Brasil inteiro e salvar na
 #' pasta 'data-raw/malha_viaria/ano/br'. O PBF pode ser baixado manualmente do
 #' site download.geofabrik.de/
@@ -12,6 +15,57 @@
 # carregar bibliotecas
 source('./R/fun/setup.R')
 library("raster")
+
+
+
+
+# checar e filtrar brazil-latest.osm.pbf ----------------------------------
+
+# year <- 2020
+
+filtrar_malha_viaria_br <- function(year) {
+ 
+  #' construir paths com a localização do Osmosis e dos arquivos PBF
+  osmosis_path <- sprintf("../../data-raw/malha_viaria/osmosis/bin/osmosis.bat")
+  
+  # PBF do Brasil inteiro
+  br_pbf <- sprintf("../../data-raw/malha_viaria/%s/br/brazil-latest.osm.pbf", year)
+  
+  # PBF do Brasil inteiro, filtrado (somente malha viária, sem polígonos)
+  br_filtered_pbf <- sprintf("../../data-raw/malha_viaria/%s/br/brazil-latest-filtered.osm.pbf", year)
+  
+  
+  # testar se o arquivo PBF do Brasil existe
+  if (!file.exists(br_pbf)) {
+    stop(sprintf("Malha viária não encontrada. Faça o download em download.geofabrik.de e salve o arquivo em %s", br_pbf))
+  }
+  
+
+  # testar se o arquivo filtrado já existe, para não repetir o processo desnecessariamente  
+  if (file.exists(br_filtered_pbf)) {
+    stop(sprintf("Malha viária filtrada já existe. Para gerar uma nova, remova o arquivo %s antes.", br_filtered_pbf))
+  }
+  
+  # Constrói linha de comando para executar o Osmosis
+  osmosis_cmd <- sprintf(
+    paste("%s --read-pbf %s",
+          "    --tf accept-ways highway=* public_transport=platform railway=platform park_ride=*",
+          "--tf accept-relations type=restriction",
+          "--used-node",
+          "--write-pbf %s"),
+          osmosis_path, br_pbf, br_filtered_pbf)
+  
+  # Chama o Osmosis
+  shell(osmosis_cmd, translate = TRUE)  
+  
+  
+}
+
+filtrar_malha_viaria_br(year = 2020)
+
+
+# função para extrair a malha viária do município -------------------------
+
 
 # muni <- "poa"
 # year <- 2020
@@ -29,7 +83,7 @@ extrai_malha_viaria <- function(muni, year) {
   osmosis_path <- sprintf("../../data-raw/malha_viaria/osmosis/bin/osmosis.bat")
   
   # PBF do Brasil inteiro
-  br_pbf <- sprintf("../../data-raw/malha_viaria/%s/br/brazil-latest.osm.pbf", year)
+  br_pbf <- sprintf("../../data-raw/malha_viaria/%s/br/brazil-latest-filtered.osm.pbf", year)
   
   # PBF do município
   muni_pbf <- sprintf("../../data-raw/malha_viaria/%s/%s/%s_%s.osm.pbf", year, muni, muni, year)
@@ -38,7 +92,7 @@ extrai_malha_viaria <- function(muni, year) {
   # Cria pasta do município, caso não exista
   muni_path <- sprintf("../../data-raw/malha_viaria/%s/%s", year, muni)
   if (!dir.exists(muni_path)) {
-    dir.create(path = muni_path)
+    dir.create(path = muni_path, recursive = TRUE)
   }
   
   # Constrói linha de comando para executar o Osmosis
@@ -48,10 +102,13 @@ extrai_malha_viaria <- function(muni, year) {
                          muni_pbf)
   
   # Chama o Osmosis
+  tictoc::tic(msg = muni)
   shell(osmosis_cmd, translate = TRUE)
+  tictoc::toc()
 }
 
 purrr::walk(munis_df$abrev_muni, extrai_malha_viaria, year = 2020)
 extrai_malha_viaria(muni = "poa", year = 2020)
+
 
 
