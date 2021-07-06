3248117255541c69c79d2fabf354ccb443d7a47c
Marcus Saraiva <marcus.saraiva@gmail.com>
2021-07-06 15:29:18 -0300

    Download automático da topografia, a partir do site da NASA Recorte da malha viária a partir da topografia atualizada

--- R/01.8-topografia.R
+++ R/01.8-topografia.R
@@ -19,10 +19,96 @@
 #' #' aba _Copy & Paste_, e as coordenadas são inseridas na função como um vetor.
 #' 
 #' 
-#' # carregar bibliotecas
-#' source('./R/fun/setup.R')
-#' 
-#' # sigla_muni <- "slz"
+
+# carregar bibliotecas
+source('./R/fun/setup.R')
+library(httr)
+library(raster)
+
+
+message("Inform username and password for https://urs.earthdata.nasa.gov")
+username <- readline("Give the username : ")
+password <- readline("Give the password : ")
+
+# username <- "user"
+# password <- "pass"
+
+
+# ano <- 2020
+# sigla_muni <- "man"
+
+download_srtm <- function(sigla_muni) {
+  # read municipality boundary
+  muni_sf <- readr::read_rds(sprintf("../../data-raw/municipios/2020/municipio_%s_2020.rds", sigla_muni))
+  
+  # muni_sf %>% mapview()
+  
+  bbox <- st_bbox(muni_sf)
+  bbox <- as.integer(bbox) - 1
+  
+  lons <- seq(floor(bbox[1]), ceiling(bbox[3]), by = 1)
+  lats <- seq(floor(bbox[2]), ceiling(bbox[4]), by = 1)
+  tiles <- expand.grid(lat = lats, lon = lons) %>%
+    mutate(hx = if_else(lon < 0, "W", "E"),
+           hy = if_else(lat < 0, "S", "N"))
+  tile = sprintf("%s%02d%s%03d", tiles$hy, abs(tiles$lat), tiles$hx, abs(tiles$lon))
+  
+  urls <- paste0("https://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11/",
+                tile, ".SRTMGL1.hgt.zip")
+
+  outputdir <- tempdir()
+  zipfiles <- paste0(outputdir, "\\", tile, ".hgt.zip")
+  rstfiles <- paste0(outputdir, "\\", tile, ".hgt")
+  
+  
+  walk2(urls, zipfiles, function(url, filename) {
+    httr::GET(url = url, 
+              authenticate(username, password),
+              write_disk(path =filename, overwrite = TRUE),
+              progress())
+  })
+  
+  walk(zipfiles, unzip, exdir = outputdir)
+  
+  rst <- map(rstfiles, raster)
+  if (length(rst) == 1) {
+    rst_layer <- rst[[1]]
+  } else {
+    rst_layer <- do.call(raster::mosaic, args = c(rst, fun = mean))
+  }
+  rst_layer_crop <- raster::crop(rst_layer, st_bbox(muni_sf))
+
+  dir.create(paste0("../../data/acesso_oport/topografia/", sigla_muni, "/"), recursive = TRUE)
+  raster::writeRaster(rst_layer_crop, 
+                      paste0("../../data/acesso_oport/topografia/", sigla_muni, "/topografia_", sigla_muni, ".tif"),
+                      overwrite = TRUE)
+  
+}
+
+
+
+download_srtm("poa")
+download_srtm("bel")
+walk(munis_list$munis_df$abrev_muni, download_srtm)
+
+
+sigla_muni="for"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #' 
 #' crop_save_raster <- function(ano, munis = "all") {
 #'   
