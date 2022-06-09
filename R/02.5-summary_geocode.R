51781c30fd7912e6ebcd06b54997ca631a4ff1b7
Kaue Braga <kauebraga90@gmail.com>
2022-06-09 17:09:18 -0300

    update landuse data

--- R/02.5-summary_geocode.R
+++ R/02.5-summary_geocode.R
@@ -2,27 +2,28 @@
 source('./R/fun/setup.R')
 
 # atividade <- 'rais'
+# atividade <- 'educacao'
 
 summary_geocode <- function(ano) {
   
   
-  siglas <- munis_list$munis_metro[ano_metro == ano] %>% unnest(code_muni) %>% select(sigla_muni = abrev_muni, code_muni)
+  siglas <- munis_list$munis_metro[ano_metro == ano] %>% tidyr::unnest(code_muni) %>% dplyr::select(sigla_muni = abrev_muni, code_muni)
   
   
   # empregos ----------------------------------------------------------------
   path_antes <- sprintf("../../data/acesso_oport/rais/%s/rais_%s_etapa3_geocoded.rds", ano, ano)
-  path_depois <- sprintf("../../data/acesso_oport/rais/%s/rais_%s_etapa4_geocoded_gmaps_gquality_corrected.rds", ano, ano)
+  path_depois <- sprintf("../../data/acesso_oport/rais/%s/rais_%s_etapa4_geocoded_gmaps_gquality_corrected_escola.rds", ano, ano)
   antes <- read_rds(path_antes)
   depois <- read_rds(path_depois)
-  antes <- antes %>% left_join(siglas %>% mutate(code_muni =   as.integer(substr(code_muni, 1, 6))), by = c("codemun" = "code_muni"))
-  depois <- depois %>% left_join(siglas %>% mutate(code_muni = as.integer(substr(code_muni, 1, 6))), by = c("code_muni" = "code_muni"))
+  antes <- antes %>% left_join(siglas %>% mutate(code_muni =   substr(code_muni, 1, 6)), by = c("codemun" = "code_muni"))
+  depois <- depois %>% left_join(siglas %>% mutate(code_muni = substr(code_muni, 1, 6)), by = c("code_muni" = "code_muni"))
   antes_cidade <- antes %>% group_by(sigla_muni) %>% summarise(n = sum(total_corrigido, na.rm = TRUE)) %>% arrange(desc(n))
   depois_cidade <- depois %>% group_by(sigla_muni) %>% summarise(n = sum(total_corrigido, na.rm = TRUE)) %>% arrange(desc(n))
   comp_empregos <- left_join(antes_cidade, depois_cidade, by = "sigla_muni", suffix = c("_antes", "_depois"))
   comp_empregos <- comp_empregos %>% mutate(perc = n_depois/n_antes) %>% mutate(atividade = "empregos")
   
   path_antes  <-  sprintf("../../data/acesso_oport/educacao/%s/educacao_%s_filter_geocoded_gmaps.rds", ano, ano)
-  path_depois <-  sprintf("../../data/acesso_oport/educacao/%s/educacao_%s_filter_geocoded_gmaps_gquality_corrected.rds", ano, ano)
+  path_depois <-  sprintf("../../data/acesso_oport/educacao/%s/educacao_%s_filter_geocoded_gmaps_gquality_corrected2.rds", ano, ano)
   antes <- read_rds(path_antes)
   depois <- read_rds(path_depois)
   antes <- antes %>% left_join(siglas, by = "code_muni")
@@ -55,7 +56,7 @@ go <- lapply(c(2017, 2018, 2019), summary_geocode) %>%
   rbindlist()
 
 go_new <- go %>%
-  select(-n_antes) %>%
+  dplyr::select(-n_antes) %>%
   pivot_wider(names_from = ano,
               values_from = n_depois:perc)
 
